498e9a7008adbd5f5dc5fc1aaa263b25499aa211
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2024-01-04 18:25:33 +0700

    update enabling cors for making request from

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for your GraphQL endpoint
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your client's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable credentials if needed (e.g., for cookies, authentication)
+  });
+
   await app.listen(9000);
 }
 bootstrap();
